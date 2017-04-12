3c0713f57cbbb852448b6705ac7d48d90a2e87a6
简玉锋 <jianyufeng@38.hn>
2017-04-12 14:53:10 +0800

    fdfs

---
+++ MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.utopia.musicutopiaandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 作者:Created by 简玉锋 on 2017/4/12 12:00
+ * 邮箱: jianyufeng@38.hn
+ * 主界面
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
