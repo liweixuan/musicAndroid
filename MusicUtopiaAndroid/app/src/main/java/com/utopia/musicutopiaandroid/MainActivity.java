ce557513f96cdfb15747d09a2530cbb76f6730e1
简玉锋 <jianyufeng@38.hn>
2017-04-12 17:41:33 +0800

    Merge branch 'master' of https://github.com/liweixuan/musicAndroid
    # Conflicts:
    #	MusicUtopiaAndroid/app/src/main/res/values/styles.xml

--- MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/MainActivity.java
+++ MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/MainActivity.java
@@ -2,7 +2,24 @@ package com.utopia.musicutopiaandroid;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.ashokvarma.bottomnavigation.BadgeItem;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 作者:Created by 简玉锋 on 2017/4/12 12:00
@@ -10,11 +27,162 @@ import android.support.v7.app.AppCompatActivity;
  * 主界面
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+    private static final String TAG = "MainActivity";
+    //底部导航
+    @BindView(R.id.bottom_navigation_bar)
+    BottomNavigationBar bottomNavigationBar;
+    //浮动小按钮
+    @BindView(R.id.fab_home)
+    FloatingActionButton floatingBtn;
+    //选择的导航 默认是第一个
+    int lastSelectedPosition = 0;
+
+    //未读消息
+    private BadgeItem numberBadgeItem;
+
+    private List<Fragment> list;
+    private InteractionFragment interactionFragment;
+    private TeachFragment teachFragment;
+    private MessageFragment messageFragment;
+    private FriendFragment friendFragment;
+    private MyFragment myFragment;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bottomNavigationBar.clearAll();//清除
+        bottomNavigationBar.setFab(floatingBtn);
+
+        numberBadgeItem = new BadgeItem()
+                .setBorderWidth(4)
+                .setBackgroundColorResource(R.color.colorAccent)
+                .setText("99")
+                .setHideOnSelect(false);
+        bottomNavigationBar
+                .setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp, "Home").setActiveColorResource(R.color.orange).setBadgeItem(numberBadgeItem))
+                .addItem(new BottomNavigationItem(R.drawable.ic_book_white_24dp, "Books").setActiveColorResource(R.color.teal))
+                .addItem(new BottomNavigationItem(R.drawable.ic_music_note_white_24dp, "Music").setActiveColorResource(R.color.blue))
+                .addItem(new BottomNavigationItem(R.drawable.ic_tv_white_24dp, "Movies & TV").setActiveColorResource(R.color.brown))
+                .addItem(new BottomNavigationItem(R.drawable.ic_videogame_asset_white_24dp, "Games").setActiveColorResource(R.color.grey))
+                .setFirstSelectedPosition(lastSelectedPosition)
+                .initialise();
+        bottomNavigationBar.setTabSelectedListener(this);
+
+    }
+
+    /**
+     * 点击事件
+     * @param v 控件
+     */
+    @OnClick(R.id.fab_home)
+    public void onClick(View v){
+        int id = v.getId();
+        switch (id){
+            case R.id.fab_home:
+                final Snackbar snackbar = Snackbar.make(getWindow().getDecorView(), "Fab Clicked", Snackbar.LENGTH_LONG);
+                snackbar.setAction("dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                snackbar.show();
+                break;
+        }
+    }
+
+    /**
+     * 隐藏所有正在显示的Fragment
+     * @param transaction
+     */
+    public void hideFragment(FragmentTransaction transaction) {
+        for (Fragment fragment : list) {
+            transaction.hide(fragment);
+        }
+    }
+    @Override
+    public void onTabSelected(int position) {
+        Log.e(TAG, "onTabSelected: " +position );
+        Snackbar.make(bottomNavigationBar,""+position,Snackbar.LENGTH_SHORT).show();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        /**
+         *每次添加之前隐藏所有正在显示的Fragment
+         *然后如果是第一次添加的话使用transaction.add();
+         *但第二次显示的时候,使用transaction.show();
+         *这样子我们就可以保存Fragment的状态了
+         */
+        hideFragment(transaction);
+        switch (position) {
+            case 0:
+                if (interactionFragment == null) {
+                    interactionFragment =  InteractionFragment.getInstance("互动");
+                    transaction.add(R.id.layFrame, interactionFragment);
+                    list.add(interactionFragment);
+                } else {
+                    transaction.show(interactionFragment);
+                }
+                break;
+            case 1:
+                if (teachFragment == null) {
+                    teachFragment = TeachFragment.getInstance("教学");
+                    transaction.add(R.id.layFrame, teachFragment);
+                    list.add(teachFragment);
+                } else {
+                    transaction.show(teachFragment);
+                }
+                break;
+            case 2:
+                if (messageFragment == null) {
+                    messageFragment = MessageFragment.getInstance("消息");
+                    transaction.add(R.id.layFrame, messageFragment);
+                    list.add(messageFragment);
+                } else {
+                    transaction.show(messageFragment);
+                }
+                break;
+            case 3:
+                if (friendFragment == null) {
+                    friendFragment = FriendFragment.getInstance("好友");
+                    transaction.add(R.id.layFrame, friendFragment);
+                    list.add(friendFragment);
+                } else {
+                    transaction.show(friendFragment);
+                }
+                break;
+            case 4:
+                if (myFragment == null) {
+                    myFragment = MyFragment.getInstance("好友");
+                    transaction.add(R.id.layFrame, myFragment);
+                    list.add(myFragment);
+                } else {
+                    transaction.show(myFragment);
+                }
+                break;
+        }
+        transaction.commit();
+
+
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+        Log.e(TAG, "onTabUnselected: "+position );
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+        Log.e(TAG, "onTabReselected: "+position );
 
     }
 }
---
+++ MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/MyFragment.java
@@ -0,0 +1,43 @@
+package com.utopia.musicutopiaandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 作者:Created by 简玉锋 on 2017/4/12 17:07
+ * 邮箱: jianyufeng@38.hn
+ */
+public class MyFragment extends Fragment {
+    @BindView(R.id.tv)
+    TextView tv;
+    private String title;
+
+    public static MyFragment getInstance(String title) {
+
+        MyFragment sInstance = new MyFragment();
+        sInstance.title = title;
+        return sInstance;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_interaction, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tv.setText(title);
+    }
+}
