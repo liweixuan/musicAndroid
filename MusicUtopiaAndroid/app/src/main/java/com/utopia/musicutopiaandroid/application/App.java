eabc9652850b7e709d0135f791bcd19f1c74e14d
jianyufeng <907507643@qq.com>
2017-04-26 03:34:19 +0800

    cx

--- MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/application/App.java
+++ MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/application/App.java
@@ -2,10 +2,15 @@ package com.utopia.musicutopiaandroid.application;
 
 import android.app.Application;
 
-import com.utopia.musicutopiaandroid.business.interaction.frament.dynamic.model.engine.Engine;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.cookie.CookieJarImpl;
+import com.zhy.http.okhttp.cookie.store.PersistentCookieStore;
+import com.zhy.http.okhttp.https.HttpsUtils;
+import com.zhy.http.okhttp.log.LoggerInterceptor;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 
 /**
  * 作者:Created by 简玉锋 on 2017/4/12 11:36
@@ -15,20 +20,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class App extends Application {
     private static App sInstance;
-    private Engine mEngine;
+
     @Override
     public void onCreate() {
         super.onCreate();
         sInstance = this;
-        mEngine = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build().create(Engine.class);
+        initOkhttpClient();
     }
+
+    /**
+     * 配置网络请求的 Okhttp配置OkhttpClient
+     */
+    private void initOkhttpClient() {
+        //Cookie(包含Session)  https://github.com/hongyangAndroid/okhttputils
+        // PersistentCookieStore //持久化cookie
+        //SerializableHttpCookie //持久化cookie
+        //MemoryCookieStore //cookie信息存在内存中
+        CookieJarImpl cookieJar = new CookieJarImpl(new PersistentCookieStore(getApplicationContext()));
+        //设置可访问所有的https网站
+        HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(null, null, null);
+        //设置具体的证书
+        //HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(证书的inputstream, null, null);
+        //双向认证
+        //HttpsUtils.getSslSocketFactory(证书的inputstream, 本地证书的inputstream,本地证书的密码)
+        //配置
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
+                .addInterceptor(new LoggerInterceptor("TAG", true))//对于Log
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+    }
+
     public static App getInstance() {
         return sInstance;
     }
-    public Engine getEngine() {
-        return mEngine;
-    }
+
 }
--- MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/business/interaction/frament/dynamic/DynamicFragment.java
+++ MusicUtopiaAndroid/app/src/main/java/com/utopia/musicutopiaandroid/business/interaction/frament/dynamic/DynamicFragment.java
@@ -1,11 +1,12 @@
 package com.utopia.musicutopiaandroid.business.interaction.frament.dynamic;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.View;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -20,29 +21,35 @@ import com.utopia.musicutopiaandroid.business.interaction.frament.dynamic.adapte
 import com.utopia.musicutopiaandroid.framework.base.fragment.BaseFragment;
 import com.utopia.musicutopiaandroid.framework.comm.util.ThreadUtil;
 import com.utopia.musicutopiaandroid.framework.comm.util.ToastUtil;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.BitmapCallback;
+import com.zhy.http.okhttp.callback.FileCallBack;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zhy.http.okhttp.request.RequestCall;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import cn.bingoogolapple.refreshlayout.BGAMoocStyleRefreshViewHolder;
 import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import okhttp3.Call;
+import okhttp3.MediaType;
 
 import static com.utopia.musicutopiaandroid.R.id.refreshLayout;
 
 /**
  * Created by Administrator on 2017/4/18/018.
+ *
+ *  compile 'com.zhy:okhttputils:2.6.2'
  */
 
 public class DynamicFragment extends BaseFragment implements BGARefreshLayout.BGARefreshLayoutDelegate {
     @BindView(refreshLayout)
-     BGARefreshLayout mRefreshLayout;
+    BGARefreshLayout mRefreshLayout;
     @BindView(R.id.recycler_view)
-     RecyclerView mContain;
+    RecyclerView mContain;
     private NormalRecyclerViewAdapter mAdapter;
     private ComunAdapter mAdapter1;
     private Dra mAdapter2;
@@ -86,13 +93,13 @@ public class DynamicFragment extends BaseFragment implements BGARefreshLayout.BG
 //        mDataRv.setLayoutManager(new LinearLayoutManager(mApp, LinearLayoutManager.VERTICAL, false));
 //        mContain.setAdapter(mAdapter1);
         mAdapter1.setDuration(1500);
-        mAdapter1.setEmptyView(true,true,View.inflate(getContext(),R.layout.text,null));
+        mAdapter1.setEmptyView(true, true, View.inflate(getContext(), R.layout.text, null));
         mAdapter1.openLoadMore(true);
         mAdapter1.openLoadAnimation(BaseQuickAdapter.SLIDEIN_RIGHT);
-        mAdapter1.addHeaderView(View.inflate(getContext(),R.layout.text1,null));
-        mAdapter1.addHeaderView(View.inflate(getContext(),R.layout.text3,null));
-        mAdapter1.addFooterView(View.inflate(getContext(),R.layout.text3,null));
-        mAdapter1.addFooterView(View.inflate(getContext(),R.layout.text,null));
+        mAdapter1.addHeaderView(View.inflate(getContext(), R.layout.text1, null));
+        mAdapter1.addHeaderView(View.inflate(getContext(), R.layout.text3, null));
+        mAdapter1.addFooterView(View.inflate(getContext(), R.layout.text3, null));
+        mAdapter1.addFooterView(View.inflate(getContext(), R.layout.text, null));
         mAdapter1.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
             @Override
             public void onLoadMoreRequested() {
@@ -103,25 +110,35 @@ public class DynamicFragment extends BaseFragment implements BGARefreshLayout.BG
         mAdapter1.setOnRecyclerViewItemClickListener(new BaseQuickAdapter.OnRecyclerViewItemClickListener() {
             @Override
             public void onItemClick(View view, int i) {
-                ToastUtil.show(":::"+i);
+                ToastUtil.show(":::" + i);
             }
         });
         OnItemDragListener onItemDragListener = new OnItemDragListener() {
             @Override
-            public void onItemDragStart(RecyclerView.ViewHolder viewHolder, int pos){}
+            public void onItemDragStart(RecyclerView.ViewHolder viewHolder, int pos) {
+            }
+
             @Override
-            public void onItemDragMoving(RecyclerView.ViewHolder source, int from, RecyclerView.ViewHolder target, int to) {}
+            public void onItemDragMoving(RecyclerView.ViewHolder source, int from, RecyclerView.ViewHolder target, int to) {
+            }
+
             @Override
-            public void onItemDragEnd(RecyclerView.ViewHolder viewHolder, int pos) {}
+            public void onItemDragEnd(RecyclerView.ViewHolder viewHolder, int pos) {
+            }
         };
 
         OnItemSwipeListener onItemSwipeListener = new OnItemSwipeListener() {
             @Override
-            public void onItemSwipeStart(RecyclerView.ViewHolder viewHolder, int pos) {}
+            public void onItemSwipeStart(RecyclerView.ViewHolder viewHolder, int pos) {
+            }
+
             @Override
-            public void clearView(RecyclerView.ViewHolder viewHolder, int pos) {}
+            public void clearView(RecyclerView.ViewHolder viewHolder, int pos) {
+            }
+
             @Override
-            public void onItemSwiped(RecyclerView.ViewHolder viewHolder, int pos) {}
+            public void onItemSwiped(RecyclerView.ViewHolder viewHolder, int pos) {
+            }
 
             @Override
             public void onItemSwipeMoving(Canvas canvas, RecyclerView.ViewHolder viewHolder, float v, float v1, boolean b) {
@@ -144,28 +161,168 @@ public class DynamicFragment extends BaseFragment implements BGARefreshLayout.BG
     }
 
 
-    List<RefreshModel>  getDates(){
-        Call<ResponseBody> call =mApp.getEngine().contributorsBySimpleGetCall("","");
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Log.d(TAG, "onResponse: "+response.toString());
-            }
+    List<RefreshModel> getDates() {
+        //GET请求
+        OkHttpUtils.get().url("")
+                .addParams("", "")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    }
 
+                    @Override
+                    public void onResponse(String response, int id) {
+
+                    }
+                });
+        //POST请求
+        OkHttpUtils
+                .post()
+                .url("")
+                .addParams("username", "hyman")
+                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+
+                    }
+                });
+        //Post JSON 提交一个Gson字符串到服务器端，
+        // 注意：传递JSON的时候，不要通过addHeader去设置contentType，
+        // 而使用.mediaType(MediaType.parse("application/json; charset=utf-8")).。
+        OkHttpUtils.postString()
+                .url("")
+                .content("")
+                .mediaType(MediaType.parse("application/json; charset=utf-8"))
+                .build()
+                .execute(null);
+        //Post File
+        //将文件作为请求体，发送到服务器
+        OkHttpUtils.postFile()
+                .url("")
+                .file(new File(""))
+                .build()
+                .execute(null);
+        //Post表单形式上传文件
+        //支持单个多个文件，addFile的第一个参数为文件的key，
+        // 即类别表单中<input type="file" name="mFile"/>的name属性。
+        OkHttpUtils.post()
+                .addFile("mFile", "messenger_01.png", null)//
+                .addFile("mFile", "test1.txt", null)//
+                .url("")
+                .addParams("", "")
+                .build()
+                .execute(null);
+        //自定义CallBack
+
+        //下载文件
+        //注意下载文件可以使用FileCallback，需要传入文件需要保存的文件夹以及文件名。
+        OkHttpUtils//
+                .get()//
+                .url("")//
+                .build()//
+                .execute(new FileCallBack(Environment.getExternalStorageDirectory().getAbsolutePath(), "gson-2.2.1.jar")//
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(File response, int id) {
+
+                    }
+
+                    @Override
+                    public void inProgress(float progress, long total, int id) {
+                        super.inProgress(progress, total, id);
+                    }
+                });
+
+
+        //显示图片
+        //显示图片，回调传入BitmapCallback即可。
+        OkHttpUtils
+                .get()//
+                .url("")//
+                .build()//
+                .execute(new BitmapCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Bitmap response, int id) {
+
+                    }
+                });
+        //上传下载的进度显示
+        /*new Callback<T>()
+        {
+            //...
+            @Override
+            public void inProgress(float progress)
+            {
+                //use progress: 0 ~ 1
             }
-        });
-        call.cancel();//取消
-        List<RefreshModel> s = new ArrayList<>() ;
+        }*/
+
+        //取消单个请求
+        RequestCall call = OkHttpUtils.get().url("").build();
+        call.cancel();
+
+
+        /*
+        * 目前对于支持的方法都添加了最后一个参数Object tag，
+        * 取消则通过OkHttpUtils.cancelTag(tag)执行。
+        *  * OkHttpUtils
+        .get()//
+        .url(url)//
+        .tag(this)//
+     .build()//
+
+        @   Override
+        protected void onDestroy()
+        {
+         super.onDestroy();
+     //可以取消同一个tag的
+     OkHttpUtils.cancelTag(this);//取消以Activity.this作为tag的请求
+
+    混淆
+     #okhttputils
+-dontwarn com.zhy.http.**
+-keep class com.zhy.http.**{*;}
+
+
+#okhttp
+-dontwarn okhttp3.**
+-keep class okhttp3.**{*;}
+
+
+#okio
+-dontwarn okio.**
+-keep class okio.**{*;}
+
+https://github.com/hongyangAndroid/okhttputils#上传下载的进度显示
+
+}*/
+        List<RefreshModel> s = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
-            s.add(new RefreshModel("item"+i,"夏:::"+i,i));
+            s.add(new RefreshModel("item" + i, "夏:::" + i, i));
         }
         return s;
-    };
+    }
 
-
+    ;
 
 
     @Override
@@ -194,10 +351,6 @@ public class DynamicFragment extends BaseFragment implements BGARefreshLayout.BG
 
         return true;
     }
-
-
-
-
 
 
 }
